436068fc26ccbf592dadaa097de93fe07635943e
frank <frank1q@163.com>
2014-06-05 21:29:18 +0800

    Conf/config.php

--- Conf/config.php
+++ Conf/config.php
@@ -1,13 +1,20 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'DB_PREFIX'		=>	'api_',
-	'DB_CHARSET'	=>	'utf8',
-	'DB_DSN'		=>	'mysql://root:root@localhost:3306/php-api',
+
 	'TMPL_L_DELIM'	=>	'<{',
 	'TMPL_R_DELIM'	=>	'}>',
 	'SHOW_PAGE_TRACE' =>true,
 	'URL_CASE_INSENSITIVE'=> true,
 
+	'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_PORT' => 3306,
+    'DB_NAME' => 'php-api',
+	'DB_PREFIX'		=>	'api_',
+	'DB_CHARSET'	=>	'utf8',
+	
 	);
 ?>
